588ce85f22d89bdf7096e21b35b9769387d2a87f
Madhav Paudel <poudelmadhav143@gmail.com>
2026-06-18 16:02:28 +0545

    setup necessary model and migrations to store user tokens

---
+++ app/models/notification_token.rb
@@ -0,0 +1,8 @@
+class NotificationToken < ApplicationRecord
+  belongs_to :user
+
+  enum :platform, { fcm: 0, apns: 1 }
+
+  validates :token, presence: true
+  validates :token, uniqueness: { scope: [:user_id, :platform] }
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_noticed_notifications
+  has_many :notification_tokens, dependent: :destroy
 end
