a5fa8a71f52a61b8a3539785db443169408ea887
Madhav Paudel <poudelmadhav143@gmail.com>
2026-06-18 17:42:40 +0545

    setup necessary things to register token

---
+++ app/controllers/notification_tokens_controller.rb
@@ -0,0 +1,24 @@
+class NotificationTokensController < ApplicationController
+  before_action :set_user
+
+  def create
+    @token = @user.notification_tokens.find_or_create_by!(notification_token_params)
+    redirect_to @user, notice: "Token registered successfully"
+  end
+
+  def destroy
+    @token = @user.notification_tokens.find(params[:id])
+    @token.destroy!
+    redirect_to @user, notice: "Token removed"
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def notification_token_params
+    params.expect(notification_token: [:token, :platform])
+  end
+end
